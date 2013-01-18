747498df7d03490d52d37569dbb225c14ab90285
sdd330 <yang.leijun@gmail.com>
2013-01-18 09:51:59 +0800

    Native: Disable maximize window

--- NativeContainer/GriffinsPortalKit/NativeContainer.Designer.cs
+++ NativeContainer/GriffinsPortalKit/NativeContainer.Designer.cs
@@ -45,6 +45,8 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "NativeContainer";
             this.Text = "Griffins Portal Kit";
             this.Load += new System.EventHandler(this.SplashScreen_Load);
