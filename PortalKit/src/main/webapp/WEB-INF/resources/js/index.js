302c2b094322f639d32494abc73b7fc3151c9bfd
zuohao <leftstick@qq.com>
2013-01-30 12:35:15 +0800

    PortalKit: update index.js for fw part, to enhance the load time

--- PortalKit/src/main/webapp/WEB-INF/resources/js/index.js
+++ PortalKit/src/main/webapp/WEB-INF/resources/js/index.js
@@ -19,51 +19,76 @@ $(document).ready(function(){
     
     var initUrl = "/init/getViewsInfo.ajax";
     var shownViewId = undefined;
-  
-    function viewsInfoLoading(ViewsInfo){
-        if(!ViewsInfo|| !ViewsInfo.length){
-            //error handle
-        }
-        var count = ViewsInfo.length;
-
-        var showViews = function(){
-             var showId = undefined;
-             for(var i =0; i < ViewsInfo.length; i++){
-                 if(ViewsInfo[i].defaultView){
-                     showId = ViewsInfo[i].viewId;
-                     break;
-                 }
-             }
-             $('.content-wrapper').addClass("contentAnimation");
-             ViewManager.show("#"+showId);
-        };
-        
-        var loadJs = function(){
-            for(var i = 0; i < ViewsInfo.length; i++){
-                var viewInfo = ViewsInfo[i];
-                DynamicLoad.loadStaticJs(viewInfo.js, function(){count--; if(count === 0){showViews();}}, function(error){});
+    var viewsInfo = undefined;
+    
+    function removeViewInfo(viewId){
+        for(var i in viewsInfo){
+            if(viewsInfo[i].viewId === viewId){
+                viewsInfo.splice(i, 1);
+                break;
             }
-        };
-        
-        var loadView = function(view){
-            count--;
-            var injector = angular.injector(['myModule', 'ng']);
-            var compiler = injector.get('compiler');
-            $('.maincontent .content-wrapper').append(compiler.compile(view));
-            ViewManager.hide(".maincontent .content-wrapper .content-box");
-            if(count === 0){
-                count = ViewsInfo.length;
-                loadJs();
-            }
-        };
-        
-        for(var i = 0; i < ViewsInfo.length; i++){
-            var viewInfo = ViewsInfo[i];
-            DynamicLoad.loadStaticHtml(viewInfo.templateUrl, loadView, function(error){});
         }
     }
     
-    DynamicLoad.loadJSON(initUrl, undefined, viewsInfoLoading, function(error){});
+    function findViewInfo(viewId){
+        if(viewsInfo.length === 0){
+            return undefined;
+        }
+        
+        for(var i in viewsInfo){
+            if(viewsInfo[i].viewId === viewId){
+                return viewsInfo[i];
+            }
+        }
+        //implicitly falsy
+    }
+    
+    function loadViewJS(view, callback){
+        if(!view){
+            return;
+        }
+        
+        var loadJS = function(){
+            DynamicLoad.loadStaticJs(view.js, function(){
+                if(callback){
+                    callback();
+                }
+                removeViewInfo(view.viewId);
+            }, function(error){ViewManager.simpleError("Load js "+view.js+ " error : " +error.message);});
+        };
+        
+        var loadView = function(html){
+            var injector = angular.injector(['myModule', 'ng']);
+            var compiler = injector.get('compiler');
+            $('.maincontent .content-wrapper').append(compiler.compile(html));
+            ViewManager.hide(".maincontent .content-wrapper .content-box");
+            loadJS();
+        };
+        
+        DynamicLoad.loadStaticHtml(view.templateUrl, loadView, function(error){ViewManager.simpleError("Load view "+view.viewId+ " error : " +error.message);});
+    }
+    
+    function viewsInfoLoad(ViewsInfo){
+        viewsInfo = ViewsInfo;
+        
+        //init default view
+        var defaultView = undefined;
+        for(var i =0; i < viewsInfo.length; i++){
+            if(viewsInfo[i].defaultView){
+                defaultView = viewsInfo[i];
+                break;
+            }
+        }
+        
+        loadViewJS(defaultView, function(){
+
+            ViewManager.show("#" + defaultView.viewId);
+            $('.maincontent .content-wrapper').addClass("contentAnimation");
+        
+        });
+    }
+  
+    DynamicLoad.loadJSON(initUrl, undefined, viewsInfoLoad, function(error){ViewManager.simpleError(error.message);});
     
 
     
@@ -78,9 +103,19 @@ $(document).ready(function(){
                     $(this).addClass('active');
                     $('.content-wrapper').removeClass("contentAnimation");
                     ViewManager.hide(".maincontent .content-wrapper .content-box");
-                    shownViewId = newId
-                    ViewManager.show("#" + shownViewId);
-                    $('.content-wrapper').addClass("contentAnimation");
+                    shownViewId = newId;
+                    var callbackOnshow = function(){
+                        ViewManager.show("#" + shownViewId);
+                        $('.maincontent .content-wrapper').addClass("contentAnimation");
+                    };
+                    
+                    var view = findViewInfo(shownViewId);
+                    if(view){
+                        loadViewJS(view, callbackOnshow);
+                    }else{
+                        callbackOnshow();
+                    }
+                   
                     return false;
     });
     
