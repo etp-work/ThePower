bb08c2ebe2571f256f7193da01dbd7bbd9d3fcf2
zuohao <leftstick@qq.com>
2013-02-05 17:50:54 +0800

    PortalKit: update embeddedTomcat

--- EmbeddedTomcat/src/main/java/org/etp/portalKit/tomcat/EmbeddedTomcat.java
+++ EmbeddedTomcat/src/main/java/org/etp/portalKit/tomcat/EmbeddedTomcat.java
@@ -1,5 +1,8 @@
 package org.etp.portalKit.tomcat;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 
 import org.apache.catalina.LifecycleException;
@@ -51,4 +54,41 @@ public class EmbeddedTomcat {
         tomcat = null;
     }
 
+    /**
+     * entrance of EmbeddedTomcat. you can set args like port=9876 to
+     * start tomcat by binding port 9876.
+     * 
+     * @param args
+     */
+    @SuppressWarnings("null")
+    public static void main(String[] args) {
+        if (args == null || args.length == 0) {
+            System.err.println("args should not be null.");
+            System.err.println("you should set them in key=value style, such as : port=9876");
+            System.exit(-1);
+        }
+        Map<String, String> parameters = new HashMap<String, String>();
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+            String[] keyvalues = arg.split("=");
+            if (keyvalues.length != 2) {
+                System.err.println("you should set arg in key=value style, such as : port=9876");
+                System.exit(-1);
+
+            }
+            String key = keyvalues[0];
+            String value = keyvalues[1];
+            parameters.put(key, value);
+        }
+
+        int tomcatPort = Integer.parseInt(parameters.get("port"));
+
+        EmbeddedTomcat tomcat = new EmbeddedTomcat(tomcatPort);
+        try {
+            tomcat.start();
+        } catch (LifecycleException | ServletException e) {
+            System.err.println(e.getMessage());
+            System.exit(-1);
+        }
+    }
 }
