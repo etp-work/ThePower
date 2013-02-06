50276f24275dbf9a3f65793dfaa628647bb8ff8f
sdd330 <yang.leijun@gmail.com>
2013-02-06 15:25:12 +0800

    Native: Change app icon

--- NativeContainer/GriffinsPortalKit/SplashForm.Designer.cs
+++ NativeContainer/GriffinsPortalKit/SplashForm.Designer.cs
@@ -45,6 +45,7 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(129, 125);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "SplashForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SplashForm";
