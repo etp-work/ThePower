d95b91b525910011ec4f42a92994b3e33c21826b
zuohao <leftstick@qq.com>
2013-06-18 08:50:09 +0800

    PortalKit: fetch testserver war file name dynamiclly

--- PortalKit/src/main/java/org/etp/portalKit/test/controller/TestController.java
+++ PortalKit/src/main/java/org/etp/portalKit/test/controller/TestController.java
@@ -1,7 +1,10 @@
 package org.etp.portalKit.test.controller;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URLEncoder;
+
+import javax.annotation.Resource;
 
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpException;
@@ -9,6 +12,10 @@ import org.apache.commons.httpclient.HttpMethodBase;
 import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.lang.StringUtils;
+import org.etp.portalKit.common.service.PropertiesManager;
+import org.etp.portalKit.common.util.FileUtils;
+import org.etp.portalKit.setting.bean.SettingsCommand;
 import org.etp.portalKit.test.bean.DataCommand;
 import org.etp.portalKit.test.bean.TestCommand;
 import org.springframework.stereotype.Controller;
@@ -23,117 +30,140 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class TestController {
 
-    private static final HttpClient httpclient = new HttpClient();
+	@Resource(name = "propertiesManager")
+	private PropertiesManager prop;
 
-    private String getDefatltIP() {
-        return "127.0.0.1";
-    }
+	private static final HttpClient httpclient = new HttpClient();
 
-    private String getDefaultPort() {
-        return "8080";
-    }
+	private String getDefatltIP() {
+		return "127.0.0.1";
+	}
 
-    private String getDefaultContextPath() {
-        return "portal-testserver-war-14.1-SNAPSHOT";
-    }
+	private String getDefaultPort() {
+		return "8080";
+	}
 
-    private String getDataRequestURL() {
-        return "public/test/data.ajax";
-    }
+	private String getDefaultContextPath() {
+		String webapps = prop.get(SettingsCommand.TOMCAT_WEBAPPS_PATH);
+		if (StringUtils.isBlank(webapps))
+			return "portal-testserver-war-14.1-SNAPSHOT";
+		File[] testservers = FileUtils.FolderFinder(webapps,
+				"portal-testserver-war");
+		return testservers[0].getName();
+	}
 
-    private String getStartTestRequestURL() {
-        return "public/test/startTest.ajax?async=ture";
-    }
+	private String getDataRequestURL() {
+		return "public/test/data.ajax";
+	}
 
-    /**
-     * @param cmd DataCommand
-     * @return auto testcases
-     * @throws HttpException error when Http exception occurs.
-     * @throws IOException error when IOException occurs.
-     */
-    @RequestMapping(value = "/test/data.ajax", method = RequestMethod.POST)
-    public @ResponseBody
-    String getData(@RequestBody DataCommand cmd) throws HttpException, IOException {
-        String requestIP = null != cmd.getTargetIP() ? cmd.getTargetIP() : getDefatltIP();
-        String requestPort = null != cmd.getTargetPort() ? cmd.getTargetPort() : getDefaultPort();
-        String requestContextPath = null != cmd.getTargetContextPath() ? cmd.getTargetContextPath()
-                : getDefaultContextPath();
-        String requestURL = String.format("http://%s:%s/%s/%s", requestIP, requestPort, requestContextPath,
-                getDataRequestURL());
-        return sendGetRequest(requestURL);
-    }
+	private String getStartTestRequestURL() {
+		return "public/test/startTest.ajax?async=ture";
+	}
 
-    /**
-     * @param cmd TestCommand
-     * @return nothing
-     * @throws HttpException error when Http exception occurs.
-     * @throws IOException error when IOException occurs.
-     */
-    @RequestMapping(value = "/test/start.ajax", method = RequestMethod.POST)
-    public @ResponseBody
-    String startTest(@RequestBody TestCommand cmd) throws HttpException, IOException {
-        String requestIP = null != cmd.getTargetIP() ? cmd.getTargetIP() : getDefatltIP();
-        String requestPort = null != cmd.getTargetPort() ? cmd.getTargetPort() : getDefaultPort();
-        String requestContextPath = null != cmd.getTargetContextPath() ? cmd.getTargetContextPath()
-                : getDefaultContextPath();
+	/**
+	 * @param cmd
+	 *            DataCommand
+	 * @return auto testcases
+	 * @throws HttpException
+	 *             error when Http exception occurs.
+	 * @throws IOException
+	 *             error when IOException occurs.
+	 */
+	@RequestMapping(value = "/test/data.ajax", method = RequestMethod.POST)
+	public @ResponseBody
+	String getData(@RequestBody DataCommand cmd) throws HttpException,
+			IOException {
+		String requestIP = null != cmd.getTargetIP() ? cmd.getTargetIP()
+				: getDefatltIP();
+		String requestPort = null != cmd.getTargetPort() ? cmd.getTargetPort()
+				: getDefaultPort();
+		String requestContextPath = null != cmd.getTargetContextPath() ? cmd
+				.getTargetContextPath() : getDefaultContextPath();
+		String requestURL = String.format("http://%s:%s/%s/%s", requestIP,
+				requestPort, requestContextPath, getDataRequestURL());
+		return sendGetRequest(requestURL);
+	}
 
-        StringBuffer cases = new StringBuffer();
-        for (String caseId : cmd.getCases()) {
-            cases.append("|" + caseId);
-        }
-        if (cases.length() > 0) {
-            cases.delete(0, 1);
-        }
+	/**
+	 * @param cmd
+	 *            TestCommand
+	 * @return nothing
+	 * @throws HttpException
+	 *             error when Http exception occurs.
+	 * @throws IOException
+	 *             error when IOException occurs.
+	 */
+	@RequestMapping(value = "/test/start.ajax", method = RequestMethod.POST)
+	public @ResponseBody
+	String startTest(@RequestBody TestCommand cmd) throws HttpException,
+			IOException {
+		String requestIP = null != cmd.getTargetIP() ? cmd.getTargetIP()
+				: getDefatltIP();
+		String requestPort = null != cmd.getTargetPort() ? cmd.getTargetPort()
+				: getDefaultPort();
+		String requestContextPath = null != cmd.getTargetContextPath() ? cmd
+				.getTargetContextPath() : getDefaultContextPath();
 
-        String requestURL = String.format("http://%s:%s/%s/%s", requestIP, requestPort, requestContextPath,
-                getStartTestRequestURL());
+		StringBuffer cases = new StringBuffer();
+		for (String caseId : cmd.getCases()) {
+			cases.append("|" + caseId);
+		}
+		if (cases.length() > 0) {
+			cases.delete(0, 1);
+		}
 
-        requestURL += "&cases=" + URLEncoder.encode(cases.toString(), "utf-8");
+		String requestURL = String.format("http://%s:%s/%s/%s", requestIP,
+				requestPort, requestContextPath, getStartTestRequestURL());
 
-        return sendGetRequest(requestURL);
-    }
+		requestURL += "&cases=" + URLEncoder.encode(cases.toString(), "utf-8");
 
-    /**
-     * @param url
-     * @return execute result
-     * @throws HttpException
-     * @throws IOException
-     */
-    protected String sendGetRequest(String url) throws HttpException, IOException {
-        return execMethod(new GetMethod(url));
-    }
+		return sendGetRequest(requestURL);
+	}
 
-    /**
-     * @param url
-     * @return execute result
-     * @throws HttpException
-     * @throws IOException
-     */
-    protected String sendPostRequest(String url) throws HttpException, IOException {
-        return execMethod(new PostMethod(url));
-    }
+	/**
+	 * @param url
+	 * @return execute result
+	 * @throws HttpException
+	 * @throws IOException
+	 */
+	protected String sendGetRequest(String url) throws HttpException,
+			IOException {
+		return execMethod(new GetMethod(url));
+	}
 
-    /**
-     * @param method
-     * @return execute result
-     * @throws HttpException
-     * @throws IOException
-     */
-    protected String execMethod(HttpMethodBase method) throws HttpException, IOException {
-        String result = null;
-        try {
-            int statusCode = httpclient.executeMethod(method);
-            if (statusCode != HttpStatus.SC_OK) {
-                throw new HttpException("Failed to get response!");
-            }
-            result = method.getResponseBodyAsString();
-        } catch (HttpException e) {
-            throw new IllegalStateException(e.toString());
-        } catch (IOException e) {
-            throw new IllegalStateException(e.toString());
-        } finally {
-            method.releaseConnection();
-        }
-        return result;
-    }
+	/**
+	 * @param url
+	 * @return execute result
+	 * @throws HttpException
+	 * @throws IOException
+	 */
+	protected String sendPostRequest(String url) throws HttpException,
+			IOException {
+		return execMethod(new PostMethod(url));
+	}
+
+	/**
+	 * @param method
+	 * @return execute result
+	 * @throws HttpException
+	 * @throws IOException
+	 */
+	protected String execMethod(HttpMethodBase method) throws HttpException,
+			IOException {
+		String result = null;
+		try {
+			int statusCode = httpclient.executeMethod(method);
+			if (statusCode != HttpStatus.SC_OK) {
+				throw new HttpException("Failed to get response!");
+			}
+			result = method.getResponseBodyAsString();
+		} catch (HttpException e) {
+			throw new IllegalStateException(e.toString());
+		} catch (IOException e) {
+			throw new IllegalStateException(e.toString());
+		} finally {
+			method.releaseConnection();
+		}
+		return result;
+	}
 }
